cb37acced293b7f4d481dd352b34149ae78a833d
jasmi <jpilav1@etf.unsa.ba>
2023-11-13 14:53:05 +0100

    5-1

---
+++ src/main/java/org/example/LicneInformacije.java
@@ -0,0 +1,22 @@
+package org.example;
+
+abstract public class LicneInformacije {
+    private String ime;
+    private String prezime;
+
+    public String getIme() {
+        return ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+}
